4c2a2e1a9bc899918a9a84c62a91d12026cd4dd9
mpadillat01 <mpadillat01@educarex.es>
2026-01-02 17:12:30 +0100

    cambios que no se cambia el calendario

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9ebee55b7c591eabfaac224d012cb4e3
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-config.js": "91e10cd1ebb7072e0e61897a67b24165",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3d49b7bac882be6ff4938f4fe6e09b18",
+"flutter_bootstrap.js": "0e6043daa6ffdd9016f675137138ac49",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
